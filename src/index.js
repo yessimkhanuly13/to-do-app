531417877c073b0bf26f80bd53d04bcbf714e108
Bekzat <esimhanuli13@gmail.com>
2023-05-19 14:32:56 +0600

    some working logic

--- src/index.js
+++ src/index.js
@@ -13,16 +13,17 @@ const inpTitle = document.querySelector('#inpTitle');
 const selectPriority = document.querySelector('#selectPriority');
 const selectProject = document.querySelector('#selectProject');
 const boxTask = document.querySelector('.task-list');
+const addTaskImg = document.querySelector('#addTaskImg');
 
 const inbox = new Project("Inbox");
 todoList.addProject(inbox);
-boxProj.innerHTML += `<li>${inbox.title}</li>`;
+boxProj.innerHTML += `<li><a href="#">${inbox.title}</a></li>`;
 
 
 function handleAddProject(){
     const project = new Project(projectInput.value);
     todoList.addProject(project);
-    const proj = `<li>${project.title}</li>`;
+    const proj = `<li><a href="#">${project.title}</a></li>`;
     const option = `<option value="${project.title}">${project.title}</option>`;
     selectProject.innerHTML += option;
     boxProj.innerHTML += proj;
@@ -38,16 +39,37 @@ function handleAddTask(){
     // </div>`;
     // boxTask.innerHTML += taskTemp;
     const project = todoList.getProjectByName(selectProject.value);
+    addTaskImg.style.display = 'flex';
+    const taskDiv = document.querySelector('.task');
+    taskDiv.style.display = 'none';
     project.addTask(task);
+    renderTasks(project);
 }
 
-// function renderTodoList(){
-
-// }
+function renderTasks(project){
+    boxTask.innerHTML = `<h3>${project.title}</h3>`;
+    project.getTasks().forEach(element => {
+        const temp = `<input type="checkbox" id=${element.title}/>
+        <label for=${element.title}>${element.title}</label>
+        `;
+        boxTask.innerHTML += temp;
+    });
+}
 
 boxProj.addEventListener('click', (e)=>{
    const project = todoList.getProjectByName(e.target.outerText);
-   console.log(project.getTasks())
+   renderTasks(project);
+//    project.getTasks().forEach(element => {
+//         const tempTask = `<li class="tasks">${element.title}</li>`;
+//         boxTask += tempTask;
+//    });
 })
+
 addProject.addEventListener('click', handleAddProject);
 addTask.addEventListener('click', handleAddTask);
+
+addTaskImg.addEventListener('click', (e)=>{
+    const task = document.querySelector('.task')
+    task.style.display = 'flex';
+    addTaskImg.style.display = 'none';
+})
